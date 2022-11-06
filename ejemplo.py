34f42d8641e2cb1b1d19589a8355ac19879d6f52
luciacantos <luciacantos22@gmail.com>
2022-11-06 18:24:48 +0100

    3

--- ejemplo.py
+++ ejemplo.py
@@ -1,23 +1,17 @@
-#número aleatorio
 import sys
 import random
-numero = random(0,100)
+numero = random.randint(0,100)
 sys.exit()
 
-#pida al usuario que introduzca un número (en una variable llamada intento)
-
-while True:
-# Se entra en un bucle infinito
-
-    # Se pide introducir un número
-    intento = input("Introduzca un número entre 1 y 99: ")
-    try:
-        intento = int(intento)
+While True:
+    numero = input("Intruduzca un número entre 1 y 99: ")
+    try: 
+        numero = int(numero)
     except:
         pass
     else:
-        # Realizar la comparación
-        if 1 <= intento <= 99:
-            # Tenemos lo que queremos, de modo que salimos del bucle
+        if 0 < numero < 100:
             break
+
 print(intento)
+
